1a834c5cbe51e736004f399b9f83998a99eb40f3
anujk2 <anujk2k2@gmail.com>
2024-03-23 21:34:41 +0530

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,13 +8,13 @@ export default function Navbar() {
 <nav className="bg-black text-white">
   <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <div className="flex items-center justify-between h-16">
-      <Link className="text-xl font-bold" href="/">Blog</Link>
+      <Link className="text-xl font-bold" to="/">Blog</Link>
       <div className="hidden md:block">
         <div className="flex ml-10 space-x-4">
-          <Link className="text-white hover:text-gray-300" href="/">Home</Link>
-          <Link className="text-white hover:text-gray-300" href="/blog">All Blogs</Link>
-          <Link className="text-white hover:text-gray-300" href="/profile">Profile</Link>
-          <Link className="text-white hover:text-gray-300" href="#">Contact Us</Link>
+          <Link className="text-white hover:text-gray-300" to="/">Home</Link>
+          <Link className="text-white hover:text-gray-300" to="/blog">All Blogs</Link>
+          <Link className="text-white hover:text-gray-300" to="/profile">Profile</Link>
+          <Link className="text-white hover:text-gray-300" to="#">Contact Us</Link>
         </div>
       </div>
     </div>
